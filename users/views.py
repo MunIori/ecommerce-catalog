910a1713fcfcd3198af75f5d42853d5751b4eb5f
Erwin Williams <erwin_mp@outlook.com>
2025-09-26 03:27:54 -0600

    feat(auth): add logout with refresh token blacklist

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
-from rest_framework import generics
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+class LogoutView(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
