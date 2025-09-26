8add6b15a3915e11e0c992986d71c814f5314321
Erwin Williams <erwin_mp@outlook.com>
2025-09-25 23:31:57 -0600

    feat(users): add serializer, view, auth urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from catalog.views import CategoryViewSet, ProductViewSet
+from users.views import RegisterView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
@@ -10,4 +17,7 @@ router.register(r'products', ProductViewSet, basename='product')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterView.as_view(), name='register'),
 ]
